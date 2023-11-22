03642ba6ac084b8d27eb9670ad5f37b168769b60
Szymon Trościński <szymon050799@o2.pl>
2023-11-22 18:02:49 +0100

    Dodaj pliki projektów.

---
+++ AplikacjaLoty/Controllers/FlightControllers.cs
@@ -0,0 +1,37 @@
+﻿using AplikacjaLoty.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+
+namespace FlightInfoAPI.Controllers
+{
+    // internetowy interfejs API
+    [ApiController]
+    [Route("api/flights")]
+    public class FlightController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory; // można zarejestrować i użyć do konfigurowania i tworzenia HttpClient
+
+        public FlightController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFlights() // obsługuje żądanie HTTP GET 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var apiKey = "1b512bb2757662df7d3396419f6e1d6c";
+            var apiUrl = $"https://api.aviationstack.com/v1/flights?access_key={apiKey}";
+
+            var response = await client.GetStringAsync(apiUrl); // operacja asynchroniczna
+            var flights = JsonSerializer.Deserialize<FlightModel>(response); // konwersji z formatu JSON do obiektu C#
+
+            { 
+            //Kod którego nie potrafię
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ AplikacjaLoty/Models/FlightModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AplikacjaLoty.Models
+{
+    public class FlightModel
+    {
+        public string AirlineName { get; set; } // Przechowuje nazwę linii lotniczej
+        public string FlightDate { get; set; }
+        public string FlightIata { get; set; } // Przechowuje oznaczenie lotu (IATA)
+        public string DepartureAirport { get; set; } // Przechowuje miejsce wylotu
+        public string DepartureIata { get; set; } // Przechowuje oznaczenie lotniska odlotu (IATA)
+        public string ArrivalAirport { get; set; } // Przechowuje nmiejsce przylotu
+        public string ArrivalIata { get; set; } // Przechowuje oznaczenie lotniska przylotu (IATA)
+        public string FlightStatus { get; set; } // Określa, czy lot jest w powietrzu
+    }
+}
